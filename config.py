e114952e783c2b93f76ef0f315f268c44f44a517
Ethan Clarke <ethan.clarke42@mail.dcu.ie>
2021-05-27 15:50:57 +0100

    Added boolean variable to disable thunking() on startup

--- config.py
+++ config.py
@@ -1,2 +1,5 @@
 # File Path to your gmod install, please paste between the quotation marks
-Gmod_Location = "C:\Program Files (x86)\Steam\SteamApps\common\GarrysMod"
+Gmod_Location = r"C:\Program Files (x86)\Steam\SteamApps\common\GarrysMod"
+
+# Set to False if you dont want the beautiful KamThunking ASCII art on startup
+KamThunking = True

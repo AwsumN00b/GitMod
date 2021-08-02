38a67a99f061fe9c246329f8cee9274081b10808
Ethan Clarke <ethan.clarke42@mail.dcu.ie>
2021-08-02 15:31:57 +0100

    GUI is visually complete. Window Icon added The GUI has all the space to add functionality right now, but functionality doesnt exist in it at all

--- GUI.py
+++ GUI.py
@@ -2,6 +2,7 @@ from tkinter import *
 
 def main():
     root = Tk()
+    root.iconbitmap("GitMod.ico")
     root.title("GitMod")
 
 
@@ -34,28 +35,59 @@ def main():
 
 
     # Big Box in middle of page
-    midFrame = Frame(root, bg="white")
-    midFrame.pack(fill=X)
+    midBoxFrame = Frame(root, bg="white")
+    midBoxFrame.pack(fill=X)
 
     # Key of table
-    midKeyTable = Label(midFrame, text="Type")
+    midKeyTable = Label(midBoxFrame, text="Type")
     midKeyTable.grid(row=0, column=0)
-    midKeyTable = Label(midFrame, text="Name")
+    midKeyTable = Label(midBoxFrame, text="Name")
     midKeyTable.grid(row=0, column=1)
-    midKeyTable = Label(midFrame, text="State")
+    midKeyTable = Label(midBoxFrame, text="State")
     midKeyTable.grid(row=0, column=2)
-    midKeyTable = Label(midFrame, text="Git Status")
+    midKeyTable = Label(midBoxFrame, text="Git Status")
     midKeyTable.grid(row=0, column=3)
 
-    midKeyTable = Label(midFrame, text="Saves:")
+    midKeyTable = Label(midBoxFrame, text="Saves:")
     midKeyTable.grid(row=1, column=0)
 
-    # function to fill in save data
+    # add function to fill in save data
 
-    midKeyTable = Label(midFrame, text="SMH:")
+    midKeyTable = Label(midBoxFrame, text="SMH:")
     midKeyTable.grid(row=2, column=0)
 
-    # function to fill in smh data
+    # add function to fill in smh data
+
+
+    # Long bar across middle section of window
+    midLongFrame = Frame(root, bg="gray80")
+    midLongFrame.pack(fill=X)
+
+    # Buttons inside midLongFrame
+    button_extract = Button(midLongFrame, text="Extract", bg="sky blue")
+    button_extract.pack(side=LEFT)
+
+    button_inject = Button(midLongFrame, text="Inject", bg="PaleGreen1")
+    button_inject.pack(side=RIGHT)
+
+
+    # Long Frame along bottom right side of window for Buttons
+    bottomRightFrame = Frame(root, bg="white")
+    bottomRightFrame.pack(side=RIGHT)
+
+    # Buttons inside bottomBoxFrame
+    button_commit = Button(bottomRightFrame, text="Commit", bg="steel blue")
+    button_commit.pack()
+
+    button_push = Button(bottomRightFrame, text="Push", bg="gold2")
+    button_push.pack(fill=X)
+
+    button_pull = Button(bottomRightFrame, text="Pull", bg="plum3")
+    button_pull.pack(fill=X)
+
+
+    # Box along bottom of screen
+    bottomBoxFrame = Frame(root, bg="white")
 
 
 
